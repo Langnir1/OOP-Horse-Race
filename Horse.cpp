db810511b2730df88f9d96b853834a9dd7be66fb
Langnir1 <joshua.halamka@bsu.edu>
2025-02-08 01:01:30 +0000

    Horse.h is estabilished and Horse.cpp is started

--- Horse.cpp
+++ Horse.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <random>
-class Horse {
-	private:
-		int position;
-		int id;
-		int raceLength;
-	public:
-		Horse();
-		void init(int id, int raceLength);
-		void advance();
-		void printLane();
-		bool isWinner();
-};
+#include <Horse.h>
+#include <cstdlib>
+
+srand(time(NULL));
+
+Horse::Horse(){
+	Horse::position = 0;
+	Horse::id = 0;
+	Horse::raceLength = 15;
+}
+
+void Horse::init(int id, int raceLength){
+	Horse::position = 0;
+	Horse::id = id;
+	Horse::raceLength = raceLength;
+}
+
+void Horse::advance(){
+	int coin = rand() % 2;
+	Horse::position  += coin;
+}
+
+void Horse::printLane(){
+	for(int i = 0; i < Horse::raceLength; i++){
+		if(i == Horse::position){
+			std::cout << Horse::id;
+		} else{
+			std::cout << ".";
+		}
+	}
+}
--- Horse.h
+++ Horse.h
@@ -1,1 +1,11 @@
-
+class Horse {
+        private:
+                int position;
+                int id;
+                int raceLength;
+        public:
+                Horse();
+                void init(int id, int raceLength);
+                void advance();
+                void printLane();
+                bool isWinner()
